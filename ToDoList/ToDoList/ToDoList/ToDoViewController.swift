1000e79cf5edd75c5b3b83aa8184380796f85685
2priyageorge <69159549+2priyageorge@users.noreply.github.com>
2021-08-02 09:29:00 -0500

    Add files via upload

---
+++ ToDoList/ToDoList/ToDoList/ToDoViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoViewController.swift
+//  ToDoList
+//
+//  Created by Scholar on 8/2/21.
+//
+
+import UIKit
+
+func createToDo () -> [ToDoClass]{
+let swiftToDo = ToDoClass ()
+    swiftToDo.description = "Learn Swift"
+    
+}
+
